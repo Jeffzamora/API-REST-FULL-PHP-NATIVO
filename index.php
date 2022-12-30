ea186d0d096cc858b065c8dca55381bf31e886a0
Jefferson Zamora <jeffzamorachiqui97@gmail.com>
2022-12-30 14:49:53 -0600

    add path

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 require_once "controller/rutasController.php";
 require_once "controller/cursosController.php";
 require_once "controller/clientesController.php";
+require_once "models/clientesModels.php";
+require_once "models/cursosModels.php";
 
 $rutas = new ControllerRuta();
 $rutas->inicio();

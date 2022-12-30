c3d6e4595ee2f3c782da7abeb2f3a9d3048c1f2a
Jefferson Zamora <jeffzamorachiqui97@gmail.com>
2022-12-30 14:49:37 -0600

    Api controller and commit

--- controller/cursosController.php
+++ controller/cursosController.php
@@ -2,14 +2,18 @@
 
 class controllerCursos{
     public function index(){
+
+        $cursos = ModelsCursos::index("cursos");
         $json = array(
-            "Detalle"=>"Estas en la Vista Cursos"
+            "Detalle"=>$cursos
         );
 
         echo json_encode($json, true);
 
         return;
     }
+
+    //Funcion para Ver cursos
     public function create(){
         $json = array(
             "Detalle"=>"Estas en la Vista CREAR cursos"
@@ -20,6 +24,7 @@ class controllerCursos{
         return;
     }
 
+    //Funcion para ver cursos por ID
     public function show($id){
         $json = array(
             "Detalle"=>"Este es el curso con el id -----".$id
@@ -30,9 +35,21 @@ class controllerCursos{
         return;
     }
 
-    public function update(){
+    //Funcion para Actualizar cursos
+    public function update($id){
         $json = array(
-            "Detalle"=>"Actualizando Curso"
+            "Detalle"=>"Actualizando Curso --->".$id
+        );
+
+        echo json_encode($json, true);
+
+        return;
+    }
+
+    //Funcion para Actualizar cursos
+    public function delete($id){
+        $json = array(
+            "Detalle"=>"Curso Borrado --->".$id
         );
 
         echo json_encode($json, true);
